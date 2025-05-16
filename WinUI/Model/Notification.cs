235f470989b9cb22dd20afdcd7f984235020299c
laithha <147397351+laithha@users.noreply.github.com>
2025-05-16 16:38:29 +0300

    fixed notification bindings

--- WinUI/Model/Notification.cs
+++ WinUI/Model/Notification.cs
@@ -8,58 +8,25 @@ namespace WinUI.Model
 {
     internal class Notification
     {
-        private int _id;
-        private int _userId;
-        private int _doctorId;
-        private string _notificationMessage;
-        private DateTime _notificationDate;
+        public int notificationId { get; set; }
+        public int userId { get; set; }
+        public string notificationMessage { get; set; }
+        public DateTime notificationDate { get; set; }
 
-        Notification(int id, int userId, int doctorId, string notificationMessage, DateTime notificationDate)
+        public Notification(int notificationId, int userId, int doctorId, string notificationMessage, DateTime notificationDate)
         {
-            this._id = id;
-            this._userId = userId;
-            this._doctorId = doctorId;
-            this._notificationMessage = notificationMessage;
-            this._notificationDate = notificationDate;
+            this.notificationId = notificationId;
+            this.userId = userId;
+            this.notificationMessage = notificationMessage;
+            this.notificationDate = notificationDate;
         }
 
-        Notification()
+        public Notification()
         {
-            this._id = 0;
-            this._userId = 0;
-            this._doctorId = 0;
-            this._notificationMessage = string.Empty;
-            this._notificationDate = DateTime.Now;
-        }
-
-        public int id
-        {
-            get { return _id; }
-            set { _id = value; }
-        }
-
-        public int userId
-        {
-            get { return _userId; }
-            set { _userId = value; }
-        }
-
-        public int doctorId
-        {
-            get { return _doctorId; }
-            set { _doctorId = value; }
-        }
-
-        public string notificationMessage
-        {
-            get { return _notificationMessage; }
-            set { _notificationMessage = value; }
-        }
-
-        public DateTime notificationDate
-        {
-            get { return _notificationDate; }
-            set { _notificationDate = value; }
+            notificationId = 0;
+            userId = 0;
+            notificationMessage = string.Empty;
+            notificationDate = DateTime.Now;
         }
     }
 }
