76afc650d1aef9d9df91bde64ccc3ce7efffd86f
Barnut Cristiana <119601150+criss05@users.noreply.github.com>
2025-05-18 14:36:16 +0300

    Add INotificationService interface for notification management

---
+++ ClassLibrary/IService/INotificationService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary.Domain;
+using ClassLibrary.IRepository;
+using ClassLibrary;
+
+namespace ClassLibrary.IService
+
+{
+    public interface INotificationService
+    {
+        /// <summary>
+        /// Return all Notifications for all users.
+        /// </summary>
+        Task<List<Notification>> getAllNotificationsAsync();
+
+        /// <summary>
+        /// Return all notifications for a specific user.
+        /// </summary>
+        Task<List<Notification>> getNotificationsByUserIdAsync(int user_id);
+
+        /// <summary>
+        /// Delete all notifications with <paramref name="notificationId"/>, 
+        /// Only if is from user <paramref name="user_id"/>.
+        /// </summary>
+        Task deleteNotificationAsync(int notificationId, int user_id);
+    }
+}
